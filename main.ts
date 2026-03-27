ad627f5152b9cbdf1ac45c8aad4dbbc796357587
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-03-27 09:01:06 -0400

    changed comments and README

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Noah
+ * Created on: March 2026
+ * This program lights up different neopixels according to distance
 */
 
 // variables needed
